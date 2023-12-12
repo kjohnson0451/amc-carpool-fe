058bc993258fc24daedf4376bc0cb3b376e37e2b
Keith Johnson <kjohnson0451@gmail.com>
2023-12-12 14:24:55 -0500

    Styled the trip list

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -14,8 +14,15 @@ function TripList() {
 
   return (
     <ul>
-      {trips.map((trip) => (
-        <li key={trip.id}>{trip.name}</li>
+      {trips.map((trip, index) => (
+        <li
+          key={trip.id}
+          className={`py-2 px-4 ${
+            index % 2 === 0 ? "bg-custom-gray-light" : "bg-custom-gray-lightest"
+          } hover:bg-purple-900`}
+        >
+          {trip.name}
+        </li>
       ))}
     </ul>
   )
