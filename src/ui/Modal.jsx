efd59523551fd5fec6c14852addb65b57465b586
Keith Johnson <kjohnson0451@gmail.com>
2024-01-02 16:33:11 -0500

    Use a React Portal to render the modal outside the app DOM This ensures that the modal isn't cut off if it's reused where `overflow: hidden` might be applied

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,7 +1,8 @@
+import { createPortal } from "react-dom"
 import { HiXMark } from "react-icons/hi2"
 
 function Modal({ children, onClose }) {
-  return (
+  return createPortal(
     <div className="fixed left-0 top-0 z-50 h-full w-full bg-gray-700 bg-opacity-50 backdrop-blur transition duration-500">
       <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-custom-gray-dark p-10 shadow-lg transition duration-500">
         <button className="absolute right-2 top-2 p-1.5" onClick={onClose}>
@@ -9,7 +10,8 @@ function Modal({ children, onClose }) {
         </button>
         <div>{children}</div>
       </div>
-    </div>
+    </div>,
+    document.body,
   )
 }
 
