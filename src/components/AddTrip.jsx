8feea62f46307ebc583d6b7b2dd667b9a2d67b4e
Keith Johnson <kjohnson0451@gmail.com>
2024-01-03 10:49:40 -0500

    Turn the modal into a compound component

--- src/components/AddTrip.jsx
+++ src/components/AddTrip.jsx
@@ -1,29 +1,41 @@
-import { useState } from "react"
 import CreateTripForm from "@components/CreateTripForm"
 import Modal from "@ui/Modal"
 
 function AddTrip() {
-  const [isOpenModal, setIsOpenModal] = useState(false)
   return (
-    <div>
-      <button type="button" onClick={() => setIsOpenModal((show) => !show)}>
-        Add new trip
-      </button>
-      {isOpenModal && (
-        <Modal
-          onClose={() => {
-            setIsOpenModal(false)
-          }}
-        >
-          <CreateTripForm
-            onCloseModal={() => {
-              setIsOpenModal(false)
-            }}
-          />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="create-trip-form">
+        <button type="button">Add new trip</button>
+      </Modal.Open>
+      <Modal.Window name="create-trip-form">
+        <CreateTripForm />
+      </Modal.Window>
+    </Modal>
   )
 }
+
+// function AddTrip() {
+//   const [isOpenModal, setIsOpenModal] = useState(false)
+//   return (
+//     <div>
+//       <button type="button" onClick={() => setIsOpenModal((show) => !show)}>
+//         Add new trip
+//       </button>
+//       {isOpenModal && (
+//         <Modal
+//           onClose={() => {
+//             setIsOpenModal(false)
+//           }}
+//         >
+//           <CreateTripForm
+//             onCloseModal={() => {
+//               setIsOpenModal(false)
+//             }}
+//           />
+//         </Modal>
+//       )}
+//     </div>
+//   )
+// }
 
 export default AddTrip
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,11 +1,47 @@
+import {
+  cloneElement,
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from "react"
 import { createPortal } from "react-dom"
 import { HiXMark } from "react-icons/hi2"
 
-function Modal({ children, onClose }) {
+const ModalContext = createContext()
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState("")
+
+  const close = useCallback(() => setOpenName(""), [])
+  const open = setOpenName
+
+  const contextValue = useMemo(
+    () => ({ openName, close, open }),
+    [openName, close, open],
+  )
+
+  return (
+    <ModalContext.Provider value={contextValue}>
+      {children}
+    </ModalContext.Provider>
+  )
+}
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext)
+  return cloneElement(children, { onClick: () => open(opensWindowName) })
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext)
+  if (name !== openName) return null
+
   return createPortal(
     <div className="fixed left-0 top-0 z-50 h-full w-full bg-gray-700 bg-opacity-50 backdrop-blur transition duration-500">
       <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-custom-gray-dark p-10 shadow-lg transition duration-500">
-        <button className="absolute right-2 top-2 p-1.5" onClick={onClose}>
+        <button className="absolute right-2 top-2 p-1.5" onClick={close}>
           <HiXMark />
         </button>
         <div>{children}</div>
@@ -14,5 +50,8 @@ function Modal({ children, onClose }) {
     document.body,
   )
 }
+
+Modal.Open = Open
+Modal.Window = Window
 
 export default Modal
