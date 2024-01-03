b5e52ec2a96665dad9c57dcf10ab6408c00fcd46
Keith Johnson <kjohnson0451@gmail.com>
2024-01-03 10:55:47 -0500

    Remove comments

--- src/components/AddTrip.jsx
+++ src/components/AddTrip.jsx
@@ -14,28 +14,4 @@ function AddTrip() {
   )
 }
 
-// function AddTrip() {
-//   const [isOpenModal, setIsOpenModal] = useState(false)
-//   return (
-//     <div>
-//       <button type="button" onClick={() => setIsOpenModal((show) => !show)}>
-//         Add new trip
-//       </button>
-//       {isOpenModal && (
-//         <Modal
-//           onClose={() => {
-//             setIsOpenModal(false)
-//           }}
-//         >
-//           <CreateTripForm
-//             onCloseModal={() => {
-//               setIsOpenModal(false)
-//             }}
-//           />
-//         </Modal>
-//       )}
-//     </div>
-//   )
-// }
-
 export default AddTrip
