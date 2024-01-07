7b39d99598a53423130be1adf257ff78dcc71788
Keith Johnson <kjohnson0451@gmail.com>
2024-01-07 18:50:25 -0500

    Revert vite.config.js back to vanilla

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-/* eslint-disable */
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@config": "/src/config",
-      "@components": "/src/components",
-      "@hooks": "/src/hooks",
-      "@pages": "/src/pages",
-      "@services": "/src/services",
-      "@ui": "/src/ui",
-      "@utils": "/src/utils",
-      // Add more aliases as needed
-    },
-  },
 })
