a505d57bdfea9e95e4c00a92fb9b47741ee33bad
Keith Johnson <kjohnson0451@gmail.com>
2024-01-07 18:48:13 -0500

    Remove everything else in src

--- src/config/queryKeys.js
+++
@@ -1,6 +0,0 @@
-const QUERY_KEYS = {
-  TRIPS: "trips",
-  TRIP_DETAILS: "trip_details",
-}
-
-export default QUERY_KEYS
--- src/pages/PageNotFound.jsx
+++
@@ -1,9 +0,0 @@
-function PageNotFound() {
-  return (
-    <div>
-      <h2>Page not found</h2>
-    </div>
-  )
-}
-
-export default PageNotFound
