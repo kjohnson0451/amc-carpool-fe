8f4db018e0ec2f8d898b846e6a8b1dc3bac9629a
Keith Johnson <kjohnson0451@gmail.com>
2024-01-08 12:22:43 -0500

    Use a label for the form

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -7,18 +7,19 @@ function Input({
   register = null,
 }) {
   return (
-    <div className="grid-cols-fr mt-4 grid grid-cols-[1fr_1.2fr] first:mt-0">
-      <span className="self-center">{label}:</span>
-      <span className="ml-1 self-center">
-        <input
-          id={id}
-          type={type}
-          placeholder={placeholder}
-          disabled={disabled}
-          {...register}
-        />
-      </span>
-    </div>
+    // This eslint rule seems to be busted
+    // eslint-disable-next-line jsx-a11y/label-has-associated-control
+    <label className="mt-2 block first:mt-0">
+      {label}
+      <input
+        className="block"
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        disabled={disabled}
+        {...register}
+      />
+    </label>
   )
 }
 
