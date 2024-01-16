dbf70d637ef316565008c24dae0a616857489436
Keith Johnson <kjohnson0451@gmail.com>
2024-01-16 15:48:19 -0500

    useOutsideClick to close modal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -9,6 +9,7 @@ import {
 import { createPortal } from "react-dom"
 import { HiXMark } from "react-icons/hi2"
 import IconButton from "@ui/IconButton"
+import useOutsideClick from "../hooks/ui/useOutsideClick"
 
 const ModalContext = createContext()
 
@@ -37,11 +38,16 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext)
+  const ref = useOutsideClick({ handler: close })
+
   if (name !== openName) return null
 
   return createPortal(
     <div className="fixed left-0 top-0 z-50 h-full w-full bg-gray-700 bg-opacity-50 backdrop-blur transition duration-500">
-      <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-custom-gray-dark p-10 shadow-lg transition duration-500">
+      <div
+        ref={ref}
+        className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-custom-gray-dark p-10 shadow-lg transition duration-500"
+      >
         <IconButton onClick={close} className="absolute right-2 top-2">
           <HiXMark />
         </IconButton>
