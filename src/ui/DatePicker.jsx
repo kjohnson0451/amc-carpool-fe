428c78695b11e04a49d8750d52fd2a072b2f5b62
Keith Johnson <kjohnson0451@gmail.com>
2024-01-16 10:18:21 -0500

    Style datepicker

--- src/ui/DatePicker.jsx
+++ src/ui/DatePicker.jsx
@@ -1,9 +1,17 @@
 import DatePickerReact from "react-datepicker"
 
 import "react-datepicker/dist/react-datepicker.css"
+import "../react-datepicker-override.css"
 
 function DatePicker({ value, onChange, id }) {
-  return <DatePickerReact id={id} selected={value} onChange={onChange} />
+  return (
+    <DatePickerReact
+      id={id}
+      selected={value}
+      onChange={onChange}
+      showPopperArrow={false}
+    />
+  )
 }
 
 export default DatePicker
