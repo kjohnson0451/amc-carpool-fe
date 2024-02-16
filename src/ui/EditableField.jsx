78c0abc086aadadc2a4c30dffd06ab192030bd7d
Keith Johnson <kjohnson0451@gmail.com>
2024-02-16 11:40:05 -0500

    Remove the functionality from EditableField

--- src/ui/EditableField.jsx
+++ src/ui/EditableField.jsx
@@ -1,54 +1,13 @@
-import { useState } from "react"
+import { FaEdit } from "react-icons/fa"
 
-function EditableField({ label, value, type, onUpdate }) {
-  const [isEditing, setIsEditing] = useState(false)
-  const [editedValue, setEditedValue] = useState(value)
-
-  function handleEditClick() {
-    setIsEditing(true)
-  }
-
-  function handleInputChange(e) {
-    setEditedValue(e.target.value)
-  }
-
-  function handleUpdate() {
-    onUpdate(editedValue)
-    setIsEditing(false)
-  }
-
-  function handleKeyDown(e) {
-    if (e.key === "Enter") {
-      handleUpdate()
-    }
-  }
-
+function EditableField({ label, value }) {
   return (
-    <div className="flex">
+    <div className="flex items-center">
       <span className="mr-1 whitespace-nowrap">{label}: </span>
-      {isEditing ? (
-        <input
-          type={type}
-          value={editedValue}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-        />
-      ) : (
-        <span className="m-w-0 truncate">{value}</span>
-      )}
-      {isEditing ? (
-        // eslint-disable-next-line max-len
-        // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
-        <span className="cursor-pointer" onClick={handleUpdate}>
-          ✓
-        </span>
-      ) : (
-        // eslint-disable-next-line max-len
-        // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
-        <span className="cursor-pointer" onClick={handleEditClick}>
-          🖉
-        </span>
-      )}
+      <span className="m-w-0 cursor-pointer truncate">{value}</span>
+      <span className="cursor-pointer pl-2.5">
+        <FaEdit />
+      </span>
     </div>
   )
 }
