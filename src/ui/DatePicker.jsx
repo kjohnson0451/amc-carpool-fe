d9e6894ca63fda137a38cdc84fdacb63d5ee044f
Keith Johnson <kjohnson0451@gmail.com>
2024-02-21 18:43:21 -0500

    Refactored the DatePicker with a useEffect so it actually registers the date

--- src/ui/DatePicker.jsx
+++ src/ui/DatePicker.jsx
@@ -1,15 +1,43 @@
+import { forwardRef, useEffect, useState } from "react"
 import DatePickerReact from "react-datepicker"
 
 import "react-datepicker/dist/react-datepicker.css"
-import "../react-datepicker-override.css"
 
-function DatePicker({ value, onChange, id }) {
+// function CustomInput({ props, ref }) {
+//   const { value } = props
+//   return <input value={value} ref={ref} />
+// }
+
+// function DatePicker({ value, id }) {
+//   const [startDate, setStartDate] = useState(new Date())
+//   const CustomInputRef = forwardRef(CustomInput)
+//   return (
+//     <DatePickerReact
+//       selected={startDate}
+//       onChange={(date) => setStartDate(date)}
+//       customInput={<CustomInputRef value={value} id={id} />}
+//     />
+//   )
+// }
+
+function CustomInput({ value, onChange, onClick }, ref) {
+  return <input value={value} onChange={onChange} onClick={onClick} ref={ref} />
+}
+
+function DatePicker({ id, onChange }) {
+  const [startDate, setStartDate] = useState(null)
+
+  useEffect(() => {
+    onChange(startDate)
+  }, [onChange, startDate])
+
+  const ExampleCustomInput = forwardRef(CustomInput)
   return (
     <DatePickerReact
       id={id}
-      selected={value}
-      onChange={onChange}
-      showPopperArrow={false}
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+      customInput={<ExampleCustomInput />}
     />
   )
 }
