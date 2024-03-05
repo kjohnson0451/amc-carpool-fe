aec796a2c5daaff95206958d27d5b24db508ec50
Keith Johnson <kjohnson0451@gmail.com>
2024-03-05 17:10:58 -0500

    Remove this commented code

--- src/ui/DatePicker.jsx
+++ src/ui/DatePicker.jsx
@@ -3,23 +3,6 @@ import DatePickerReact from "react-datepicker"
 
 import "react-datepicker/dist/react-datepicker.css"
 
-// function CustomInput({ props, ref }) {
-//   const { value } = props
-//   return <input value={value} ref={ref} />
-// }
-
-// function DatePicker({ value, id }) {
-//   const [startDate, setStartDate] = useState(new Date())
-//   const CustomInputRef = forwardRef(CustomInput)
-//   return (
-//     <DatePickerReact
-//       selected={startDate}
-//       onChange={(date) => setStartDate(date)}
-//       customInput={<CustomInputRef value={value} id={id} />}
-//     />
-//   )
-// }
-
 function CustomInput({ value, onChange, onClick }, ref) {
   return <input value={value} onChange={onChange} onClick={onClick} ref={ref} />
 }
