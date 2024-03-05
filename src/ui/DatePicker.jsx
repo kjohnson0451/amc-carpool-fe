51ab184e936c6a8b35b06ddb723b82b1ab22e9a9
Keith Johnson <kjohnson0451@gmail.com>
2024-03-05 17:17:01 -0500

    Only a little override is necessary

--- src/ui/DatePicker.jsx
+++ src/ui/DatePicker.jsx
@@ -2,9 +2,18 @@ import { forwardRef, useEffect, useState } from "react"
 import DatePickerReact from "react-datepicker"
 
 import "react-datepicker/dist/react-datepicker.css"
+import "../react-datepicker-override.css"
 
 function CustomInput({ value, onChange, onClick }, ref) {
-  return <input value={value} onChange={onChange} onClick={onClick} ref={ref} />
+  return (
+    <input
+      className="block w-full rounded-md border-0 bg-gray-900 py-1.5 pl-2 text-sm leading-6 text-gray-100 outline-none ring-1 ring-inset ring-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+      value={value}
+      onChange={onChange}
+      onClick={onClick}
+      ref={ref}
+    />
+  )
 }
 
 function DatePicker({ id, onChange }) {
